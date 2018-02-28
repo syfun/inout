4445b28dc133b71c813b416424465d2abc293a1b
syfun <sunyu418@gmail.com>
2018-02-28 22:22:18 +0800

    add react-router and layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
 import './App.css'
+import { Layout } from 'antd'
+import LeftMenu from './components/LeftMenu'
+import Label from './components/Label'
+import Push from './components/Push'
+import Pop from './components/Pop'
+import Stock from './components/Stock'
+
+const { Content, Footer, Sider } = Layout
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Layout>
+          <Sider style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0 }}>
+            {/* <div className='logo' /> */}
+            <LeftMenu />
+          </Sider>
+          <Layout style={{ marginLeft: 200 }}>
+            <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+              <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
+                <Route exact path='/' component={Push} />
+                <Route path='/pop' component={Pop} />
+                <Route path='/stock' component={Stock} />
+                <Route path='/setting' component={Label} />
+              </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>
+        Ant Design ©2016 Created by Ant UED
+            </Footer>
+          </Layout>
+        </Layout>
+      </Router>
     )
   }
 }
---
+++ src/components/LeftMenu.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import { Menu, Icon } from 'antd'
+
+import { NavLink } from 'react-router-dom'
+
+class LeftMenu extends Component {
+  render () {
+    return (
+      <div style={{ width: 256 }}>
+        <Menu
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode='inline'
+          theme='dark'
+          inlineCollapsed={false}
+        >
+          <Menu.Item key='1'>
+            <NavLink to='/'>
+              <Icon type='pie-chart' />
+              <span>入库</span>
+            </NavLink>
+
+          </Menu.Item>
+          <Menu.Item key='2'>
+            <NavLink to='/pop'>
+              <Icon type='desktop' />
+              <span>出库</span>
+            </NavLink>
+          </Menu.Item>
+          <Menu.Item key='3'>
+            <NavLink to='/stock'>
+              <Icon type='inbox' />
+              <span>库存统计</span>
+            </NavLink>
+          </Menu.Item>
+          <Menu.Item key='4'>
+            <NavLink to='/setting'>
+              <Icon type='inbox' />
+              <span>设置</span>
+            </NavLink>
+          </Menu.Item>
+        </Menu>
+      </div>
+    )
+  }
+}
+
+export default LeftMenu
